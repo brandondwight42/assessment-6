ab773c1fbf563c82fd5a4d44f536f1c4661a1a60
Brandon Hunsaker <brandondwight42@gmail.com>
2022-02-25 14:59:22 -0700

    write automated tests attempted

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,15 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('Clicking the Draw button displays the div with id=choices', async () => {
+    const choices = await driver.findElement(By.id('choices'))
+    const displayed = await choices.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Clicking the Add to Duo button displays the div with id=player-duo', async () => {
+    const player_Duo = await driver.findElement(By.id('player_Duo'))
+    const displayed = await player_Duo.isDisplayed()
+    expect(displayed).toBe(true)
+})
